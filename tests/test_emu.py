10c8c1844b2b0799c2f5329a956bbef9995d75b7
Samuel Lampa <samuel.lampa@scilifelab.se>
2024-12-18 14:53:07 +0100

    Refactor out output basepath creation into separate function and add unit test

--- tests/test_emu.py
+++ tests/test_emu.py
@@ -1,6 +1,38 @@
-def test_placeholder():
-    """
-    Add an empty test so as not to fail the pytest process until some real
-    tests have been added.
-    """
-    assert True
+import os
+import pytest
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import emu
+
+
+@pytest.mark.parametrize(
+    "output_dir, output_basename, input_file, expected_output_basepath",
+    [
+        (
+            "results",  # output_dir
+            "",  # output_basename
+            "some_file.fastq",  # input_file
+            "results/some_file.fastq",  # expected_output_basepath
+        ),
+        (
+            "results",  # similar as above ...
+            "",
+            ["some_file.fastq", "another_file.fastq"],
+            "results/some_file-another_file",
+        ),
+        (
+            "results",
+            "my-output-basename",
+            "some_file.fastq",
+            "results/my-output-basename",
+        ),
+    ],
+)
+def test_generate_outfile_path(
+    output_dir, output_basename, input_file, expected_output_basepath
+):
+    got_output_basepath = emu.create_output_basepath(
+        output_dir, output_basename, input_file
+    )
+    assert got_output_basepath == expected_output_basepath
