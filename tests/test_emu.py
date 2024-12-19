8b003bc00ca08ddb0fe92f2ea97333e61a39f4f7
Samuel Lampa <samuel.lampa@scilifelab.se>
2024-12-19 10:25:20 +0100

    Break up generate alignments function into a few testable functions

--- tests/test_emu.py
+++ tests/test_emu.py
@@ -29,10 +29,104 @@ import emu
         ),
     ],
 )
-def test_generate_outfile_path(
+def test_create_output_basepath(
     output_dir, output_basename, input_file, expected_output_basepath
 ):
     got_output_basepath = emu.create_output_basepath(
         output_dir, output_basename, input_file
     )
     assert got_output_basepath == expected_output_basepath
+
+
+@pytest.mark.parametrize(
+    "in_file_list, database, preset, num_threads, max_sec_alignments, minibatch_size, optional_args, output_file, expected_mm2_command",
+    [
+        (
+            ["foo1.fastq", "foo2.fastq"],
+            "emu_database",
+            "map-ont",
+            "4",
+            "5",
+            "500M",
+            "",
+            "out.sam",
+            "minimap2 -ax map-ont -t 4 -N 5 -p .9 -K 500M emu_database/species_taxid.fasta foo1.fastq foo2.fastq -o out.sam",
+        ),
+        (
+            ["foo1.fastq", "foo2.fastq"],
+            "custom_db",
+            "map-ont",
+            "10",
+            "1",
+            "100M",
+            True,
+            "myoutput.sam",
+            "minimap2 -ax map-ont -t 10 -N 1 -p .9 -u f -K 100M custom_db/species_taxid.fasta foo1.fastq foo2.fastq -o myoutput.sam",
+        ),
+    ],
+)
+def test_create_minimap2_command(
+    in_file_list,
+    database,
+    preset,
+    num_threads,
+    max_sec_alignments,
+    minibatch_size,
+    optional_args,
+    output_file,
+    expected_mm2_command,
+):
+    got_mm2_command = emu.create_minimap2_command(
+        in_file_list,
+        database,
+        preset,
+        num_threads,
+        max_sec_alignments,
+        minibatch_size,
+        optional_args,
+        output_file,
+    )
+    assert expected_mm2_command == got_mm2_command
+
+
+@pytest.mark.parametrize(
+    "file_path, expected_bool",
+    [
+        ("myfile.sam", True),
+        ("myfile.txt", False),
+        ("myfile.sam.py", False),
+    ],
+)
+def test_is_sam_file(file_path, expected_bool):
+    is_sam = emu.is_sam_file(file_path)
+    assert is_sam == expected_bool
+
+
+@pytest.mark.parametrize(
+    "file_path, expected_bool",
+    [
+        ("myfile.fastq", True),
+        ("myfile.fq", True),
+        ("myfile.fastq.txt", False),
+    ],
+)
+def test_is_fastq_file(file_path, expected_bool):
+    is_fastq = emu.is_fastq_file(file_path)
+    assert is_fastq == expected_bool
+
+
+def test_generate_alignments():
+    sam_file = "results/somesamfile.sam"
+
+    got_alignment_file = emu.generate_alignments(
+        in_file_list=[sam_file],
+        out_basename="mybasepath",
+        database="emu_database",
+        mm2_preset="map-ont",
+        num_threads="4",
+        max_sec_alignments="4",
+        minibatch_size="500M",
+        mm2_fwd_only=False,
+    )
+
+    assert got_alignment_file == sam_file
