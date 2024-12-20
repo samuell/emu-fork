b5555b851564e8c2b129072fff5a6bc31948ab98
Samuel Lampa <samuel.lampa@scilifelab.se>
2024-12-20 16:43:33 +0100

    Use tempfile for test output directory instead of custom one

--- tests/test_emu_cli.py
+++ tests/test_emu_cli.py
@@ -2,14 +2,15 @@ import pytest
 import subprocess
 import os
 import pandas as pd
+import tempfile
 
 
 @pytest.mark.parametrize(
-    "emu_command, abundance_file, expected_abundancies",
+    "emu_command_tpl, abundance_filepath_tpl, expected_abundancies",
     [
         (
-            "./emu abundance --db emu_database --output-dir test_results --keep-files example/full_length.fa",
-            "test_results/full_length_rel-abundance.tsv",
+            "./emu abundance --db emu_database --output-dir {tempdir_path} --keep-files example/full_length.fa",
+            "{tempdir_path}/full_length_rel-abundance.tsv",
             {
                 "Sphingobacterium puteale": 0.5,
                 "Mycobacterium saskatchewanense": 0.25,
@@ -17,15 +18,15 @@ import pandas as pd
             },
         ),
         (
-            "./emu abundance --db emu_database --output-dir test_results --type sr --keep-files example/short_read_f.fq",
-            "test_results/short_read_f_rel-abundance.tsv",
+            "./emu abundance --db emu_database --output-dir {tempdir_path} --type sr --keep-files example/short_read_f.fq",
+            "{tempdir_path}/short_read_f_rel-abundance.tsv",
             {
                 "Staphylococcus hominis": 1.0,
             },
         ),
         (
-            "./emu abundance --db emu_database --output-dir test_results --type sr --keep-files example/short_read_f.fq example/short_read_r.fq",
-            "test_results/short_read_f-short_read_r_rel-abundance.tsv",
+            "./emu abundance --db emu_database --output-dir {tempdir_path} --type sr --keep-files example/short_read_f.fq example/short_read_r.fq",
+            "{tempdir_path}/short_read_f-short_read_r_rel-abundance.tsv",
             {
                 "Staphylococcus aureus": 1.0 / 3,
                 "Salmonella enterica": 1.0 / 3,
@@ -34,15 +35,20 @@ import pandas as pd
         ),
     ],
 )
-def test_cli_on_example_datasets(emu_command, abundance_file, expected_abundancies):
-    if os.path.exists(abundance_file):
-        os.remove(abundance_file)
+def test_cli_on_example_datasets(emu_command_tpl, abundance_filepath_tpl, expected_abundancies):
+    tempdir = tempfile.TemporaryDirectory()
+
+    emu_command = emu_command_tpl.format(tempdir_path=tempdir.name)
+    abundance_filepath = abundance_filepath_tpl.format(tempdir_path=tempdir.name)
+
+    if os.path.exists(abundance_filepath):
+        os.remove(abundance_filepath)
 
     subprocess.check_output(emu_command, shell=True)
 
-    assert os.path.exists(abundance_file)
+    assert os.path.exists(abundance_filepath)
 
-    df = abundance_df = pd.read_csv(abundance_file, sep="\t")
+    df = abundance_df = pd.read_csv(abundance_filepath, sep="\t")
 
     for species, expected_abundance in expected_abundancies.items():
         assert df[df["species"] == species].iloc[0]["abundance"] == expected_abundance
